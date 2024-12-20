af8114e361821c00d03dbdc0ff723c7983cddf16
spe <spe@spes-MBP.home>
2024-12-20 15:02:52 -0800

    Refactor

---
+++ custom.el
@@ -0,0 +1,6 @@
+;;; Custom Variables and Faces (Generated)
+(custom-set-variables
+ '(package-selected-packages
+   '(corfu embark-consult gptel lsp-pyright magit marginalia nov
+           orderless pdf-tools rust-mode vertico zenburn-theme)))
+(custom-set-faces)
